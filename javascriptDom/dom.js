74a2bf97b4beb76a34a42da7939c47e2599e87da
Tiago Melo Menezes De Brito <tiagoinvestsalvador@gmail.com>
2023-12-18 20:57:30 -0300

    Learning click event

--- javascriptDom/dom.js
+++ javascriptDom/dom.js
@@ -273,3 +273,47 @@ btn.addEventListener('click', function() {
   heading.classList.add('red');
 });
  */
+
+
+/* 
+const btn = document.querySelector(".btn");
+const heading = document.querySelector("h2");
+
+function changeColors() {
+  let hasClass = heading.classList.contains("red");
+  if (hasClass) {
+    heading.classList.remove("red");
+  } else {
+    heading.classList.add("red");
+  }
+}
+
+btn.addEventListener("click", changeColors); */
+
+
+// click - fires after full action occurs
+// mousedown - button is pressed
+// mouseup - button is released
+// mouseenter - moved onto an element
+// mouseleave - moved out of an element
+
+/* const heading = document.querySelector('h1');
+const btn = document.querySelector('.btn');
+
+btn.addEventListener('click', function() {
+  console.log('you clicked me');
+});
+btn.addEventListener('mousedown', function() {
+  console.log('down');
+});
+btn.addEventListener('mouseup', function() {
+  console.log('up');
+});
+
+heading.addEventListener('mouseenter', function() {
+  heading.classList.add('blue');
+});
+heading.addEventListener('mouseleave', function() {
+  heading.classList.remove('blue');
+});
+ */
